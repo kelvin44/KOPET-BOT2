d3be687ef82fe3215785b95a125aae4d64cb0210
kelvin44 <66330608+kelvin44@users.noreply.github.com>
2021-02-07 04:17:37 +0700

    Update

--- plugins/calc.js
+++ plugins/calc.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, text }) => {
     console.log(val)
     let result = (new Function('return ' + val))()
     if (!result) throw result
-    m.reply(`*${format}* = _${result}_`)
+    m.reply(`${result}`)
   } catch (e) {
     if (e == undefined) throw 'Isinya?'
     throw 'Format salah, hanya 0-9 dan Simbol -, +, *, /, ×, ÷, π, e, (, ) yang disupport'
