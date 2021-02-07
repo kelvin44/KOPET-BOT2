6fc681d71184f3ba8e5e340cebf0a45e1778452d
kelvin44 <66330608+kelvin44@users.noreply.github.com>
2021-02-07 13:42:34 +0700

    Update setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -8,6 +8,7 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
 }
 handler.help = ['setwelcome <teks>']
 handler.tags = ['owner', 'group']
+handler.owner = true
 
 handler.command = /^setwelcome$/i
 module.exports = handler
